5fa2bb0946510da77cd62e898af41d993b531a30
teundejong1 <58029921+teundejong1@users.noreply.github.com>
2021-01-21 15:32:44 +0100

    Test map, werkt

---
+++ Web/test.php
@@ -0,0 +1,17 @@
+<html>
+<img src="C:\Users\teund\IdeaProjects\RandomIT-Weathertime\Web\src\img\map.png"   alt="Pacific map" width=1145 height=835 usemap="#map">
+<map name="map">
+<area href="https://www.youtube.com/watch?v=DLzxrzFCyOs" alt="NEVER" target="_blank"  shape=poly coords="98,192, 170,192, 90,275, 180,300">
+<area href="https://www.youtube.com/watch?v=DLzxrzFCyOs" alt="GONNA" target="_blank"  shape=poly coords="375,166, 435,166, 375,215, 435,215">
+<area href="https://www.youtube.com/watch?v=DLzxrzFCyOs" alt="GIVE" target="_blank"  shape=poly coords="428,265, 550,265, 489,303, 550,303">
+<area href="https://www.youtube.com/watch?v=DLzxrzFCyOs" alt="YOU" target="_blank"  shape=poly coords="708,68, 778,49, 703,132, 798,136">
+<area href="https://www.youtube.com/watch?v=DLzxrzFCyOs" alt="UP" target="_blank"  shape=poly coords="142,460, 210,472, 111,530, 207,545">
+</map>
+<?php
+
+echo"<br>";
+echo"1145 835";
+echo"<br>";
+echo"https://www.youtube.com/watch?v=DLzxrzFCyOs";
+?>
+</html>
