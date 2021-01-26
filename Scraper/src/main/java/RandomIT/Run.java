fd9c2b078797abd94e843e21e177a8170621f3a2
RedFirebreak <sjilderda@gmail.com>
2021-01-26 11:01:49 +0100

    Add global queue's

--- Scraper/src/main/java/RandomIT/Run.java
+++ Scraper/src/main/java/RandomIT/Run.java
@@ -1,22 +1,27 @@
 package RandomIT;
 
+import java.util.Queue;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Run {
+    public static final Queue<String> rawinput        = new LinkedList<String>();
+    public static final Queue<HashMap> validinput     = new LinkedList<HashMap>();
+    public static final Queue<HashMap> filteredinput  = new LinkedList<HashMap>();
+
     public static void main( String[] args ) {
         /** Reciever thread */
         System.out.println( "[LOG] Starting receiver thread" );
         DataReceiver datareceiver = new DataReceiver("Receiver 1");
         
-
         /** Parser thread */
         System.out.println( "[LOG] Starting parser thread" );
         Parser parser = new Parser("Parser 1");
         
-
         /** Filter thread */
         System.out.println( "[LOG] Starting filter thread" );
         Filter filter = new Filter("Filter 1");
         
-
         /** Store thread */
         System.out.println( "[LOG] Starting datastorage thread" );
         DataStorage datastorage = new DataStorage("Datastorage 1");
