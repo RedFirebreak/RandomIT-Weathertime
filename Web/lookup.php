d6fdd1251ed8019fb9993237dbc495562adb7e0e
Romano-Brax <58029962+Romano-Brax@users.noreply.github.com>
2021-01-30 20:21:03 +0100

    Receiving & passing data working
    Receiving data according to station-ID and passing for graphdata.

--- Web/lookup.php
+++ Web/lookup.php
@@ -24,56 +24,69 @@
     <?php
 
     require "$ROOTPATH/src/functions.php";
-    //print_r(retrieveData("HIER KOMT JE GET VOOR ", 913340 , 1, 'Windspeed'));
 
     //Data opvragen via functie, GET voor het stations-ID, dag 1, DataType.
     //Integer data
-    $retrievedDataTemperature = retrieveData($_GET['id'], 1, 'Temperature');
-    $retrievedDataDewPoint = retrieveData($_GET['id'], 1, 'DewPoint');
-    $retrievedDataPercipitation = retrieveData($_GET['id'], 1, 'Percipitation');
-    $retrievedDataSnowDrop = retrieveData($_GET['id'], 1, 'SnowDrop');
-    $retrievedDataWindspeed = retrieveData($_GET['id'], 1, 'Windspeed');
-    $retrievedDataWindDirection = retrieveData($_GET['id'], 1, 'WindDirection');
-    $retrievedDataCloudCoverage = retrieveData($_GET['id'], 1, 'CloudCoverage');
-    $retrievedDataVisibility = retrieveData($_GET['id'], 1, 'Visibility');
-    $retrievedDataStationPressure = retrieveData($_GET['id'], 1, 'StationLevelPressure');
-    $retrievedDataSeaPressure = retrieveData($_GET['id'], 1, 'SeaLevelPressure');
+    $retrievedDataTemperature = json_encode(retrieveData($_GET['id'], 1, 'Temperature'));
+    $retrievedDataDewPoint = json_encode(retrieveData($_GET['id'], 1, 'DewPoint'));
+    $retrievedDataPercipitation = json_encode(retrieveData($_GET['id'], 1, 'Percipitation'));
+    $retrievedDataSnowDrop = json_encode(retrieveData($_GET['id'], 1, 'SnowDrop'));
+    $retrievedDataWindspeed = json_encode(retrieveData($_GET['id'], 1, 'Windspeed'));
+    $retrievedDataWindDirection = json_encode(retrieveData($_GET['id'], 1, 'WindDirection'));
+    $retrievedDataCloudCoverage = json_encode(retrieveData($_GET['id'], 1, 'CloudCoverage'));
+    $retrievedDataVisibility = json_encode(retrieveData($_GET['id'], 1, 'Visibility'));
+    $retrievedDataStationPressure = json_encode(retrieveData($_GET['id'], 1, 'StationLevelPressure'));
+    $retrievedDataSeaPressure = json_encode(retrieveData($_GET['id'], 1, 'SeaLevelPressure'));
 
     //Boolean data
-    $retrievedDataStorm = retrieveData($_GET['id'], 1, 'Storm');
-    $retrievedDataSnow = retrieveData($_GET['id'], 1, 'Snow');
-    $retrievedDataRain = retrieveData($_GET['id'], 1, 'Rain');
-    $retrievedDataTornado = retrieveData($_GET['id'], 1, 'Tornado');
-    $retrievedDataFreeze = retrieveData($_GET['id'], 1, 'Freeze');
-    $retrievedDataHail = retrieveData($_GET['id'], 1, 'Hail');
+    $retrievedDataStorm = json_encode(retrieveData($_GET['id'], 1, 'Storm'));
+    $retrievedDataSnow = json_encode(retrieveData($_GET['id'], 1, 'Snow'));
+    $retrievedDataRain = json_encode(retrieveData($_GET['id'], 1, 'Rain'));
+    $retrievedDataTornado = json_encode(retrieveData($_GET['id'], 1, 'Tornado'));
+    $retrievedDataFreeze = json_encode(retrieveData($_GET['id'], 1, 'Freeze'));
+    $retrievedDataHail = json_encode(retrieveData($_GET['id'], 1, 'Hail'));
 
     ?>
 
+    <div id="test">
     <script type="text/javascript"> 
+        function toFloat(arrayDataString){
+            for (i=0; i<arrayDataString.length; i++) {
+                arrayDataString[i] = parseFloat(arrayDataString[i]);
+            }
+            return arrayDataString;
+        }
+        
         //Integer data
-        var passedDataArrayTemperature =  <?php echo '["' . implode('", "', $retrievedDataTemperature) . '"]'; ?>; 
-        var passedDataArrayDewPoint =  <?php echo '["' . implode('", "', $retrievedDataDewPoiny) . '"]'; ?>; 
-        var passedDataArrayPercipitation =  <?php echo '["' . implode('", "', $retrievedDataPercipitation) . '"]'; ?>; 
-        var passedDataArraySnowdrop =  <?php echo '["' . implode('", "', $retrievedDataSnowDrop) . '"]'; ?>; 
-        var passedDataArrayWindspeed =  <?php echo '["' . implode('", "', $retrievedDataWindspeed) . '"]'; ?>; 
-        var passedDataArrayWindDirection =  <?php echo '["' . implode('", "', $retrievedDataWindDirection) . '"]'; ?>; 
-        var passedDataArrayCloudCoverage =  <?php echo '["' . implode('", "', $retrievedDataCloudCoverage) . '"]'; ?>; 
-        var passedDataArrayVisibility =  <?php echo '["' . implode('", "', $retrievedDataVisibility) . '"]'; ?>; 
-        var passedDataArrayStationPressure =  <?php echo '["' . implode('", "', $retrievedDataStationPressure) . '"]'; ?>; 
-        var passedDataArraySeaPressure =  <?php echo '["' . implode('", "', $retrievedDataSeaPressure) . '"]'; ?>; 
+        var passedDataArrayTemperature =  JSON.parse(toFloat('<?php echo $retrievedDataTemperature; ?>')); 
+        var passedDataArrayDewPoint =  JSON.parse(toFloat('<?php echo $retrievedDataDewPoint; ?>'));
+        var passedDataArrayPercipitation =  JSON.parse(toFloat('<?php echo $retrievedDataPercipitation; ?>'));
+        var passedDataArraySnowdrop =  JSON.parse(toFloat('<?php echo $retrievedDataSnowDrop; ?>'));
+        var passedDataArrayWindspeed =  JSON.parse(toFloat('<?php echo $retrievedDataWindspeed; ?>'));
+        var passedDataArrayWindDirection =  JSON.parse(toFloat('<?php echo $retrievedDataWindDirection; ?>'));
+        var passedDataArrayCloudCoverage =  JSON.parse(toFloat('<?php echo $retrievedDataCloudCoverage; ?>'));
+        var passedDataArrayVisibility =  JSON.parse(toFloat('<?php echo $retrievedDataVisibility; ?>')); 
+        var passedDataArrayStationPressure =  JSON.parse(toFloat('<?php echo $retrievedDataStationPressure; ?>'));
+        var passedDataArraySeaPressure =  JSON.parse(toFloat('<?php echo $retrievedDataSeaPressure; ?>')); 
 
         //Boolean data
-        var passedDataArrayStorm =  <?php echo '["' . implode('", "', $retrievedDataStorm) . '"]'; ?>; 
-        var passedDataArraySnow =  <?php echo '["' . implode('", "', $retrievedDataSnow) . '"]'; ?>; 
-        var passedDataArrayRain =  <?php echo '["' . implode('", "', $retrievedDataRain) . '"]'; ?>; 
-        var passedDataArrayTornado =  <?php echo '["' . implode('", "', $retrievedDataTornado) . '"]'; ?>;
-        var passedDataArrayFreeze =  <?php echo '["' . implode('", "', $retrievedDataFreeze) . '"]'; ?>; 
-        var passedDataArrayHail =  <?php echo '["' . implode('", "', $retrievedDataHail) . '"]'; ?>;  
+        var passedDataArrayStorm =  JSON.parse('<?php echo $retrievedDataStorm; ?>');
+        var passedDataArraySnow =  JSON.parse('<?php echo $retrievedDataSnow; ?>'); 
+        var passedDataArrayRain =  JSON.parse('<?php echo $retrievedDataRain; ?>'); 
+        var passedDataArrayTornado =  JSON.parse('<?php echo $retrievedDataTornado; ?>');
+        var passedDataArrayFreeze = JSON.parse('<?php echo $retrievedDataFreeze; ?>');
+        var passedDataArrayHail =  JSON.parse('<?php echo $retrievedDataHail; ?>'); 
         
-        // Printing the passed array elements for test
-        document.write(passedDataArrayTemperature); 
-    </script>
+        //Printing the passed array elements for test
+        document.write(passedDataArrayTemperature  + "<br>");
+        document.write(passedDataArrayDewPoint  + "<br>");
+        document.write(passedDataArrayWindDirection  + "<br>");
+        document.write(passedDataArrayFreeze  + "<br>");
+        document.write(passedDataArrayStorm  + "<br>");
 
+
+    </script>
+    </div>
 </body>
 
 <footer>
